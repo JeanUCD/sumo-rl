a3f3fa551746d2168426a0cf8b8cb68ac81cca21
Lucas Alegre <lucasnale@gmail.com>
2019-04-17 17:21:35 -0300

    4x4 grid back

--- experiments/ql_4x4grid.py
+++ experiments/ql_4x4grid.py
@@ -23,39 +23,38 @@ if __name__ == '__main__':
     runs = 1
 
     env = SumoEnvironment(net_file='nets/4x4-Lucas/4x4.net.xml',
-                          route_file='nets/4x4-Lucas/4x4c2.rou.xml',
+                          route_file='nets/4x4-Lucas/4x4c1c2c1c2.rou.xml',
+                          out_csv_name='nets/4x4-Lucas/ql-c1c2c1c2-disc10',
                           use_gui=True,
-                          num_seconds=40000,
+                          num_seconds=80000,
                           time_to_load_vehicles=300,
                           max_depart_delay=0,
                           phases=[
-                            traci.trafficlight.Phase(35000, 35000, 35000, "GGGrrr"),   # north-south
-                            traci.trafficlight.Phase(2000, 2000, 2000, "yyyrrr"),
-                            traci.trafficlight.Phase(35000, 35000, 35000, "rrrGGG"),   # west-east
-                            traci.trafficlight.Phase(2000, 2000, 2000, "rrryyy")
+                            traci.trafficlight.Phase(35000, "GGGrrr"),   # north-south
+                            traci.trafficlight.Phase(2000, "yyyrrr"),
+                            traci.trafficlight.Phase(35000, "rrrGGG"),   # west-east
+                            traci.trafficlight.Phase(2000, "rrryyy")
                             ])
 
+    initial_states = env.reset()
     for run in range(1, runs+1):
-        initial_states = env.reset()
         ql_agents = {ts: QLAgent(starting_state=env.encode(initial_states[ts]),
                                  state_space=env.observation_space,
                                  action_space=env.action_space,
                                  alpha=alpha,
                                  gamma=gamma,
                                  exploration_strategy=EpsilonGreedy(initial_epsilon=0.05, min_epsilon=0.005, decay=decay)) for ts in env.ts_ids}
-        infos = []
+
         done = {'__all__': False}
         while not done['__all__']:
             actions = {ts: ql_agents[ts].act() for ts in ql_agents.keys()}
 
-            s, r, done, info = env.step(actions=actions)
-            infos.append(info)
+            s, r, done, _ = env.step(actions=actions)
 
             for agent_id in ql_agents.keys():
                 ql_agents[agent_id].learn(new_state=env.encode(s[agent_id]), reward=r[agent_id])
-
+        
         env.close()
+        initial_states = env.reset()
 
-        df = pd.DataFrame(infos)
-        df.to_csv('outputs/4x4grid/c2_alpha{}_gamma{}_decay{}_run{}.csv'.format(alpha, gamma, decay, run), index=False)
 
